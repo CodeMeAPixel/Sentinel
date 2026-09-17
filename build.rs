5114fa558798505ecd3bb2df62482c1e5e9f7d68
TheRealToxicDev <toxic.dev09@gmail.com>
2026-09-17 15:42:04 -0600

    idk at this point

--- build.rs
+++ build.rs
@@ -1,8 +1,20 @@
 extern crate vergen;
 use anyhow::Result;
 use vergen::*;
+use std::path::Path;
 
 fn main() -> Result<()> {
+    if !Path::new(".git").exists() {
+        println!("cargo:warning=Git metadata unavailable; using fallback build metadata");
+        println!("cargo:rustc-env=VERGEN_GIT_SHA=unknown");
+        println!("cargo:rustc-env=VERGEN_GIT_SEMVER=unknown");
+        println!("cargo:rustc-env=VERGEN_GIT_COMMIT_MESSAGE=unknown");
+        println!("cargo:rustc-env=VERGEN_SYSINFO_CPU_BRAND=unknown");
+        println!("cargo:rustc-env=VERGEN_CARGO_PROFILE={}", std::env::var("PROFILE").unwrap_or_else(|_| "unknown".into()));
+        println!("cargo:rustc-env=VERGEN_RUSTC_SEMVER=unknown");
+        return Ok(());
+    }
+
     let mut config = Config::default();
 
     *config.git_mut().sha_kind_mut() = ShaKind::Normal;
